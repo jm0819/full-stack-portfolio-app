12e7c2e58cfc540314bf8edebefa407e5ca27e26
Jose Mari Herrera <herrerajmjm@gmail.com>
2026-08-27 19:02:33 +0800

    render deployment prep

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # The connection string to your local PostgreSQL database
 # Format: postgresql://username:password@localhost/database_name
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123@localhost/portfolio_db"
+URL = "postgresql://postgres:123@localhost/portfolio_db"
 
+if URL.startswith("postgres://"):
+    URL = URL.replace("postgres://", "postgresql://", 1)
+    
+SQLALCHEMY_DATABASE_URL = URL
 # Create the engine that drives the connection
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
